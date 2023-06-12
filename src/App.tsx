8ba3f0625b5a1180d5e12c9f962e1bd5a8bc63a3
Adrian Valdepenas <valdepenas.adrian@gmail.com>
2023-06-12 16:56:18 +0800

    updated global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 
-import './App.css'
 import Card from './components/Card'
 function App() {
 
--- src/styles/Card.styled.tsx
+++ src/styles/Card.styled.tsx
@@ -1,23 +1,24 @@
 import styled from 'styled-components';
 
 export const CardContainer = styled.div`
-  background-color: #fff;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  padding: 16px;
-
+    background-color: #fff;
+    border-radius: 8px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    padding: 16px;
+    min-width: 30vw;
     @media (max-width: 768px) {
     padding: 8px;
+    min-width: 100%;
   }
 `;
 
 export const CardTitle = styled.h3`
-  color: #333;
-  font-size: 20px;
-  margin: 0;
+    color: #333;
+    font-size: 20px;
+    margin: 0;
 `;
 
 export const CardContent = styled.p`
-  color: #666;
-  font-size: 16px;
+    color: #666;
+    font-size: 16px;
 `;
